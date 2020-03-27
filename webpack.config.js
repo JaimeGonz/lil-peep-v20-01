36e937ecf95c14e9c47552f0a351ffc6e7931ff8
Rogelio Alatorre <jrag.tkd@gmail.com>
2020-03-27 01:17:26 -0600

    Add stimulus configuration
    Stimulus separates our logic by `controllers`. This configuration allows
    us to load all our Stimulus controllers inside the application.

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,17 @@ module.exports = {
           'sass-loader',
         ],
       },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
     ],
   },
 };
